967ff9bdf3360c7dfd09a9f62043c8893b4cb29a
Abigail <aroberts5@mail.immaculata.edu>
2022-02-16 21:11:17 -0500

    lab4

---
+++ lab4.py
@@ -0,0 +1,24 @@
+"""
+Abigail Roberts
+"""
+
+my_string = ( input ("Please type a word"))
+
+    #This line is a comment
+
+my_dict = { }
+my_dict["mother"] ="A woman in relation to her child or children."
+for mother in my_dict:
+    print("A woman in relation to her child or children.")
+my_dict["zebra"] ="An African wild horse with black and white strips."
+for zebra in my_dict:
+    print ("An African wild horse with black and white strips.")
+my_dict["penny"] ="One cent is one hundreth of a dollar."
+for penny in my_dict:
+    print ("One cent is one hundreth of a dollar.")
+else: print ("Would you like to add it?")
+
+    #This line is a comment
+
+print (my_string)
+
