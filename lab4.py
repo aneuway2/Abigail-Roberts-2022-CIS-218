89305cffa980ab6e1ad0923afdae2619240dd45e
Abigail <aroberts5@mail.immaculata.edu>
2022-02-21 14:32:53 -0500

    revision

--- lab4.py
+++ lab4.py
@@ -2,23 +2,24 @@
 Abigail Roberts
 """
 
-my_string = ( input ("Please type a word"))
+my_dict = {
+    "mother": "A woman in relation to her child",
+    "zebra": "An African wild horse",
+    "penny": "One cent is one hundredth of a dollar",
+}
+
+while True:
+
+    user_input = ( input ("Please type in a word"))
 
     #This line is a comment
 
-my_dict = { }
-my_dict["mother"] ="A woman in relation to her child or children."
-for mother in my_dict:
-    print("A woman in relation to her child or children.")
-my_dict["zebra"] ="An African wild horse with black and white strips."
-for zebra in my_dict:
-    print ("An African wild horse with black and white strips.")
-my_dict["penny"] ="One cent is one hundreth of a dollar."
-for penny in my_dict:
-    print ("One cent is one hundreth of a dollar.")
-else: print ("Would you like to add it?")
-
-    #This line is a comment
-
-print (my_string)
-
+    if my_dict.get(user_input):
+        print (my_dict.get(user_input))
+    else:
+        new_input = ( input ("Would you like to add?"))
+        if new_input == "yes":
+            new_input = "flower"
+        my_dict.update({"mother":
+"flower"})
+        print ("Updated")
