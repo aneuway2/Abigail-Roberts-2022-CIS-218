5b97e47428b8716abec4bac29a8c6813712993d6
Abigail <aroberts5@mail.immaculata.edu>
2022-03-26 21:35:23 -0400

    revision

--- lab7.py
+++ lab7.py
@@ -4,32 +4,33 @@ Abigail Roberts
 height = float(input('enter height in inches: '))
 weight = float(input('enter weight in lbs: '))
 
-def BMI(height, weight):
+def body_mass_index(height, weight):
     bmi = weight/(height**2) * 703
-    if (bmi < 18.5):
+    if bmi < 18.5:
         return 'Underweight', bmi
 
-    elif (bmi >= 18.5 and bmi < 25):
+    elif bmi >= 18.5 and bmi < 25:
         return 'Normal'
 
-    elif (bmi >= 25 and bmi < 30):
+    elif bmi >= 25 and bmi < 30:
         return 'Overweight', bmi
 
-    elif (bmi >= 30):
+    elif bmi >= 30:
         return 'Obese', bmi
 
-def check():
-    def test_check():
-        assert check.check (82, 160)==16.7
-        assert check.check (78, 280)==Obese
+def test_category():
+    assert body_mass_index(78, 280)=='Obese'
+    
+def test_bmi():
+    assert body_mass_index(82, 160)==16.7
 
 def main():
     test_check()
     print ("all ok")
 
-if __name__=="--main--":
+if __name__=="__main__":
     main()
 
-quote, bmi = BMI(height, weight)
+quote, bmi = body_mass_index(height, weight)
 print ('your bmi is: {} and you are: {}' .format(bmi, quote))
 
