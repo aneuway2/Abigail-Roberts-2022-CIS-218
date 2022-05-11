8e06798c342786ed55fa5389ab84064deaf167fa
Abigail <aroberts5@mail.immaculata.edu>
2022-05-10 22:30:43 -0400

    revision

--- lab12.py
+++ lab12.py
@@ -7,15 +7,20 @@ import sqlite3
 def __init__(self):
     con = sqlite3.connect(":memory.gradebook:")
     cur = con.cursor()
-    cur.execute("""CREATE TABLE gradebook(student, grade)""")
-    gradebook_studentgrades = [
+    cur.execute("create table lang (student, first_appeared)")
+
+    cur.execute("insert into lang values (?,?)", ("Greg", 46))
+
+    lang_list = [
         ('Samantha', 97),
         ('Tommy', 84),
         ('Tonya', 76),
         ('George', 90),
         ('Everly', 62)
         ]
-    cur.executemany("insert into gradebook values (?, ?)", gradebook_student)
-    con.commit()
-    print('SELECT* FROM gradebook')
+    cur.executemany("insert into gradebook values (?, ?)", lang_list)
+
+    cur.execute("select * from lang where firdt_appeared=:grade", {"grade":46})
+    print(cur.fetchall())
+
     con.close()
